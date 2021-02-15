b88eeee0473891e83f39cef947d7f34c737d3ef3
jodelajo <jodelajo@gmail.com>
2021-02-15 19:01:02 +0100

    refresh button

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-async function getCountryInfo(){
-    const inputElement = document.getElementById('input');
-    const userInput = inputElement.value;
-    console.log(userInput)
-    let country = userInput;
-    const url = (`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
-    const response = await axios.get(url);
-    const countryData = response.data[0];
-    const subArea = response.data[0].region;
-    const populationOfCountry = response.data[0].population;
-    const basicInfoCountryString = (`${countryData.name} is situated in ${subArea}. It has a population of ${populationOfCountry} people.`);
-    const capital = response.data[0].capital;
-    const capitalString = (`The capital is ${capital} `)
-    const currencies = countryData.currencies;
-    const languagesOfCountry = countryData.languages;
-    const afkorting = countryData.alpha3Code;
-    const afkOnderkast = afkorting.toLowerCase();
-    const flagUrl = (`https://restcountries.eu/data/${afkOnderkast}.svg`);
-    const currencyString = currency(currencies)
-    const languageString = languages(languagesOfCountry)
-    const image = document.getElementById('flag');
-    image.setAttribute('class', 'flagImage')
-    image.src = flagUrl;
-    const countryName = document.getElementById('country');
-    countryName.textContent = country;
-    const geoInfo = document.getElementById('geo');
-    geoInfo.textContent = basicInfoCountryString;
-    const capCurInfo = document.getElementById('cap');
-    capCurInfo.textContent = capitalString + currencyString;
-    const langInfo = document.getElementById('lang')
-    langInfo.textContent = languageString;
+async function getCountryInfo() {
+const refreshButton = document.getElementById("refresh")
+    try {
+        refreshButton.setAttribute('class', 'refresh-button')
+        const inputElement = document.getElementById('input');
+        let country = inputElement.value;
+        const url = (`https://restcountries.eu/rest/v2/name/${country}?fullText=true`);
+        const response = await axios.get(url);
+        console.log(response)
+        const countryData = response.data[0];
+        console.log(countryData)
+        const subArea = response.data[0].region;
+        const populationOfCountry = response.data[0].population;
+        const basicInfoCountryString = (`${countryData.name} is situated in ${subArea}. It has a population of ${populationOfCountry} people.`);
+        const capital = response.data[0].capital;
+        const capitalString = (`The capital is ${capital} `)
+        const currencies = countryData.currencies;
+        const languagesOfCountry = countryData.languages;
+        const afkorting = countryData.alpha3Code;
+        const afkOnderkast = afkorting.toLowerCase();
+        const flagUrl = (`https://restcountries.eu/data/${afkOnderkast}.svg`);
+        const currencyString = currency(currencies)
+        const languageString = languages(languagesOfCountry)
+        const image = document.getElementById('flag');
+        image.setAttribute('class', 'flagImage')
+        image.src = flagUrl;
+        const countryName = document.getElementById('country');
+        countryName.textContent = country;
+        const geoInfo = document.getElementById('geo');
+        geoInfo.textContent = basicInfoCountryString;
+        const capCurInfo = document.getElementById('cap');
+        capCurInfo.textContent = capitalString + currencyString;
+        const langInfo = document.getElementById('lang')
+        langInfo.textContent = languageString;
+    } catch (e) {
+        console.log("ERROR:", e.message);
+        console.log('ERROR RESPONSE:', e.response);
+        if (e.message === "Request failed with status code 404") {
+            console.log("404!")
+            const message = "Failed to recognize country name.";
+            const error = document.getElementById("error-message")
+            error.textContent = message;
+            refreshButton.removeAttribute('class');
+        }
+        refreshButton.addEventListener('click', reloadThePage);
+    }
 }
 
 function currency(currencyArray) {
@@ -61,6 +76,10 @@ function countryInput(event) {
     if (event.code === "Enter") {
         getCountryInfo()
     }
+}
+
+function reloadThePage(){
+    window.location.reload();
 }
 
 const pushbutton = document.getElementById("button");
