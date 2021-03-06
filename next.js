df4f4e2ebd6b9c4c18be8703f6d594cd4b4efa63
jodelajo <jodelajo@gmail.com>
2021-03-06 15:48:35 +0100

    klaar

--- next.js
+++ next.js
@@ -5,35 +5,81 @@
 // [x] - destructuring
 // [x] - sort landen op population
 
+const countryList = document.getElementById('listOfCountries');
+
 async function countriesOfTheWorld () {
     try {
     result = await axios.get(`https://restcountries.eu/rest/v2/all`);
     const { data } = result;
     console.log(data)
+
         data.sort((a, b) => {
-        return a.population - b.population;
-         });
-    countryNames(data)
+            return a.population - b.population;
+        });
+        // const countries = result.data;
+        data.map((country) => {
+            const {name, flag, population, region} = country;
+            // console.log(flag)
+            // console.log(name)
+            // console.log(region)
+
+            const singleCountry = document.createElement('li');
+            singleCountry.setAttribute('class', 'country-item')
+
+
+            const displayFlags = document.createElement('img');
+            // displayFlags.setAttribute('src', 'flag')
+            displayFlags.src = flag;
+            displayFlags.setAttribute('class', 'single-flag')
+            singleCountry.appendChild(displayFlags)
+
+
+            const displayCountryNames = document.createElement('span');
+            displayCountryNames.setAttribute('id', 'countryName')
+            displayCountryNames.setAttribute('class', colorsOfContinents(region))
+
+            displayCountryNames.textContent = name;
+            singleCountry.appendChild(displayCountryNames)
+
+            const displayPopulation = document.createElement('p')
+            displayPopulation.setAttribute('class', 'population');
+            displayPopulation.textContent = `${name} has a population of ${population} people`;
+            singleCountry.appendChild(displayPopulation);
+
+            singleCountry.addEventListener('click', () => {
+                toggleVisibility(displayPopulation);
+            });
+
+            countryList.appendChild(singleCountry)
+            // colorsOfContinents(region)
+        });
+
+
     } catch (e) {
         console.error(e);
             }
 }
-countriesOfTheWorld()
+
+countriesOfTheWorld();
 
 
-//lijst met landnamen uit de database
-// [ ] - functie maken
-// [ ] - loggen
-// [ ] - for loop maken
-// [
-function countryNames (arrayOfCountries) {
-    let country = '';
-    for (let i = 0; i < arrayOfCountries.length; i++) {
-
-        country = arrayOfCountries[i].name;
-        console.log(country)
-    }
-
+function colorsOfContinents (currentRegion) {
+   switch (currentRegion) {
+       case 'Asia':
+           return 'red';
+       case 'Europe':
+           return 'yellow';
+       case 'Africa':
+           return 'blue';
+       case 'Americas':
+           return 'green';
+       case 'Oceania':
+           return 'purple';
+       default:
+           return 'default';
+   }
 }
 
-
+function toggleVisibility(populationElement) {
+    populationElement.classList.toggle('show-population');
+}
